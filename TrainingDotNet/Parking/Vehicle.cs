8be71f43a2a9eb76e6ec0ce7409a6e65547df097
Brian David Tafur Londoño <Brian.tafur@yuxiglobal.com>
2017-06-14 15:58:54 -0500

    model and vehicle controller

---
+++ TrainingDotNet/Parking/Vehicle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parking
+{
+    class Vehicle
+    {
+
+        private String plate;
+        private int hourIn;
+        private int hourOut;
+        private String state;
+
+        public Vehicle(string plate, int hourIn)
+        {
+            this.plate = plate;
+            this.hourIn = hourIn;
+            this.state = "Inside the parking lot";
+        }
+
+        public string Plate { get => plate; set => plate = value; }
+        public int HourIn { get => hourIn; set => hourIn = value; }
+        public int HourOut { get => hourOut; set => hourOut = value; }
+        public string State { get => state; set => state = value; }
+    }
+}
---
+++ TrainingDotNet/Parking/VehicleController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parking
+{
+    class VehicleController
+    {
+
+        LinkedList<Vehicle> vehicles = new LinkedList<Vehicle>();
+
+        public VehicleController()
+        {
+        }
+
+        public bool add(Vehicle veh)
+        {
+            Vehicle temp = search(veh.Plate);
+            if (temp == null)
+            {
+                vehicles.AddLast(veh);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public Vehicle search(String plate)
+        {
+            foreach (Vehicle item in vehicles)
+            {
+                if (item.Plate.Equals(plate))
+                {
+                    return item;
+                }
+            }
+            return null;
+        }
+
+        public bool edit(Vehicle veh)
+        {
+            Vehicle temp = search(veh.Plate);
+            if (temp != null)
+            {
+                temp.HourIn = veh.HourIn;
+                temp.HourOut = veh.HourOut;
+                temp.State = veh.State;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool remove(Vehicle veh)
+        {
+            vehicles.Remove(veh);
+            return true;
+        }
+
+    }
+}
